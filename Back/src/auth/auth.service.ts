4ebfeeafba06b527fbf527ff17425da6da1b2253
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-07-17 19:14:36 +0200

    feat: adding regex for email login and password register

--- Back/src/auth/auth.service.ts
+++ Back/src/auth/auth.service.ts
@@ -15,21 +15,28 @@ export class AuthService {
     }
 
     async loginUser(email: string, password: string, confirmPassword: string): Promise<User> {
-        const newEmail = email.toLowerCase();
-        const user = await this.userService.getUserByEmail(newEmail);
+        const validateEmail = email.toLowerCase().match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        );
+        const lowerEmail = email.toLowerCase();
+        const user = await this.userService.getUserByEmail(lowerEmail);
 
-        if (!user) {
-            throw new Error("L'email ou le mot de passe sont incorrect");
+        if (!validateEmail) {
+            throw new Error("La valeur saisie dans le champ 'email' n'est pas un email");
         } else {
-            if (password !== confirmPassword) {
-                throw new Error("Les mots de passe ne correspondent pas");
+            if (!user) {
+                throw new Error("L'email ou le mot de passe est incorrect");
             } else {
-                const isCorrectPassword = await this.passwordService.comparePasswords(password, user.password);
-
-                if (!isCorrectPassword) {
-                    throw new Error("L'email ou le mot de passe sont incorrect");
+                if (password !== confirmPassword) {
+                    throw new Error("Les mots de passe ne correspondent pas");
                 } else {
-                    return user;
+                    const isCorrectPassword = await this.passwordService.comparePasswords(password, user.password);
+
+                    if (!isCorrectPassword) {
+                        throw new Error("L'email ou le mot de passe est incorrect");
+                    } else {
+                        return user;
+                    }
                 }
             }
         }
@@ -37,15 +44,21 @@ export class AuthService {
 
     async registerUser(lastname: string, firstname: string, email: string, password: string, role: Role = "USER"): Promise<User> {
         const newEmail = email.toLowerCase();
+        const newLastname = lastname.charAt(0).toUpperCase() + lastname.slice(1).toLowerCase();
+        const newFirstname = firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase();
+
         const user = await this.userService.getUserByEmail(newEmail);
 
         if (!user) {
-            const newLastname = lastname.charAt(0).toUpperCase() + lastname.slice(1).toLowerCase();
-            const newFirstname = firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase();
+            const validatePassword = password.match(/^(?=.*\d)(?=.*[!@#$%§£=+/.;,?^&*_-])(?=.*[a-z])(?=.*[A-Z]).{12,}$/);
 
-            return this.userService.createUser(newLastname, newFirstname, newEmail, password, role);
+            if (!validatePassword) {
+                throw new Error("Le mot de passe ne correspond pas. Il doit contenir au minimum : 12 caractères, 1 lettre minuscule, 1 lettre majuscule, 1 chiffre et 1 symbole");
+            } else {
+                return this.userService.createUser(newLastname, newFirstname, newEmail, password, role);
+            }
         } else {
-            throw new Error("L'email existe déjà");
+            throw new Error("Cette email est déjà utilisé");
         }
 
     }
