97eeff0a0f9a8e557e017c6efd67d91a7f4c8273
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-09-15 21:59:15 +0200

    style: hover profile picture feat: adding info span when modification is not save

--- AppWeb/src/components/Form/ProfileForm/ProfileForm.tsx
+++ AppWeb/src/components/Form/ProfileForm/ProfileForm.tsx
@@ -1,6 +1,8 @@
 "use client";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import Image from "next/image";
+import {AddCircleIcon} from "@/components/Icons/AddCircleIcon";
+import {WarningIcon} from "@/components/Icons/WarningIcon";
 
 export default function ProfileForm({session}: any) {
     const [lastname, setLastname] = useState(session.user.lastname);
@@ -8,14 +10,26 @@ export default function ProfileForm({session}: any) {
     const [email, setEmail] = useState(session.user.email);
     const [description, setDescription] = useState(session.user.description);
     const [imgProfile, setImgProfile] = useState(session.user.imgProfile);
+    const refFile: any = useRef(0);
 
     return (
         <form className="w-full flex flex-col items-center justify-start gap-12">
-            <Image src={imgProfile}
-                   alt="Photo de profil"
-                   width={2048}
-                   height={2048}
-                   className="rounded-full h-24 w-24 object-cover"/>
+            <input type="file"
+                   ref={refFile}
+                   hidden/>
+            <div className="group relative hover:cursor-pointer"
+                 onClick={() => {
+                     refFile.current.click();
+                 }}>
+                <div className="group-hover:blur-sm transition">
+                    <Image src={imgProfile}
+                           alt="Photo de profil"
+                           width={256}
+                           height={256}
+                           className="rounded-full h-24 w-24 object-cover"/>
+                </div>
+                <AddCircleIcon className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:block transition z-10 h-12 w-12"/>
+            </div>
             <div className="flex flex-col items-center gap-6 w-full">
                 <div className="flex flex-col md:flex-row gap-6 w-full">
                     <input type="text"
@@ -39,6 +53,10 @@ export default function ProfileForm({session}: any) {
                           onChange={(e) => setDescription(e.target.value)}
                           className="placeholder:text-gray-400 rounded-xl bg-gray-200 px-4 py-3 w-full"
                           rows={8}/>
+                <div className="flex items-center justify-start gap-1 w-full">
+                    <WarningIcon className="[&_path]:fill-amber-600"/>
+                    <span className="text-sm text-amber-600">Des modifications ne sont pas enregistrées</span>
+                </div>
                 <button className="bg-green-600 w-fit px-4 py-2 rounded-full transition hover:bg-green-700 active:scale-95">Sauvegarder</button>
             </div>
         </form>
--- AppWeb/src/components/MenuBar/TopMenu.tsx
+++ AppWeb/src/components/MenuBar/TopMenu.tsx
@@ -18,8 +18,8 @@ export default async function TopMenu() {
                       className="hover:opacity-75 transition">
                     <Image src={session.user.imgProfile}
                            alt="Photo de profil"
-                           width={512}
-                           height={512}
+                           width={256}
+                           height={256}
                            className="rounded-full h-8 w-8 lg:w-12 lg:h-12 object-cover"/>
                 </Link>
                 <span className="hidden lg:inline-block text-white text-sm">{session.user.lastname} {session.user.firstname}</span>
