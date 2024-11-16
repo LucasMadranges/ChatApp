fef3d81071fd707d50522ba95e2dcb68e5a0705d
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-11-16 14:20:11 +0100

    feat(Buttons, Modal, ProfileForm): Update buttons and modal components
    Adding id for each part of the Modal.tsx to better recognize
    Change default padding on Modal.tsx and delete default padding on Buttons.tsx
    Adding bg- or text- on Buttons.tsx for just choose the color instead of the tailwind class
    Adding cropper on modal and functionnality for change the profil picture when button done modal is clicked
    Adding new button for ProfileForm.tsx for stop all changes input

--- App/AppWeb/src/components/Modal/Modal.tsx
+++ App/AppWeb/src/components/Modal/Modal.tsx
@@ -24,18 +24,21 @@ export default function Modal({
     return (
         isShow && <div className="absolute top-0 left-0 w-svw h-svh bg-black bg-opacity-50"
                        onClick={handleCloseModal}>
-            <div className="rounded-2xl bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col min-w-fit w-1/2 min-h-fit h-1/2">
-                <div className={`relative border-b border-gray-200 py-4 px-8 flex items-center ${headerClass}`}>
+            <div className="rounded-2xl bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col min-w-fit w-1/2 min-h-fit max-h-svh h-3/4">
+                <div id="header"
+                     className={`relative border-b border-gray-200 p-4 flex items-center ${headerClass}`}>
                     {header || <h2>Default header!</h2>}
                     <Buttons handleClick={handleCloseModal}
                              className="absolute top-1/2 -translate-y-1/2 right-4">
                         <CloseIcon/>
                     </Buttons>
                 </div>
-                <div className={`flex-1 py-4 px-8 ${bodyClass}`}>
+                <div id="body"
+                     className={`flex-1 p-4 max-h-svh overflow-auto ${bodyClass}`}>
                     {body || <h2>Default body!</h2>}
                 </div>
-                <div className={`border-t border-gray-200 px-8 py-4 flex items-center ${footerClass}`}>
+                <div id="footer"
+                     className={`border-t border-gray-200 p-4 flex items-center ${footerClass}`}>
                     {footer || <h2>Default footer!</h2>}
                 </div>
             </div>
